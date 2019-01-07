157f1855d742712c4803d2069879d99da6a32788
Kelly Sheddy <jungaka07@gmail.com>
2019-01-07 18:57:37 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,16 @@
+
+
+
+  def patients
+    appointments.map {|appointment| appointment.patient}
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+end
+
 class Doctor
   attr_accessor :name 
   @@all = []
